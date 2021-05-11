b532bfb904c46127bccfccf4e55d689cb9ea9a86
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-05-11 22:19:18 +0200

    Add Slug to Shop

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,15 @@
 class Shop < ApplicationRecord
     has_many :business_hours, dependent: :delete_all
     validates :name, presence: true
+    after_validation :set_slug, only: [:create, :update]
+
+    private
+
+    def set_slug
+      self.slug = (name.to_s + '-'+ rand(1...20).to_s).parameterize
+    end
+  
+    def to_param
+      slug
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_11_165841) do
+ActiveRecord::Schema.define(version: 2021_05_11_201754) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2021_05_11_165841) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
   end
 
   add_foreign_key "business_hours", "shops"

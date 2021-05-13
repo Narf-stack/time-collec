0549acdfd47156025d99297be0210c6d14236878
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-05-13 22:27:04 +0200

    Update - Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ Shop.destroy_all if Rails.env.development?
 
 
 puts 'Creating shops ...'
-# 1.times do
+5.times do
   shop = Shop.new(name: Faker::Book.title )
   shop.save!
-# end
+end
 puts 'Done shops...'
 
 now = Time.now
@@ -23,61 +23,34 @@ t2 = Time.new(now.year, now.month, now.day, 14, 00, 0, 0)
 
 puts 'Business hours...'
 puts 'Mercredi...'
-a = BusinessHour.new(day:3,open:morning, close:noon)
-a.shop = shop
-a.save
-b = BusinessHour.new(day:3,open:afternoon, close:evening)
-b.shop = shop
-b.save
+BusinessHour.create(day:3,open:morning, close:noon, shop: Shop.first)
+BusinessHour.create(day:3,open:afternoon, close:evening, shop: Shop.first)
 
 puts 'Jeudi...'
-c = BusinessHour.new(day:4,open:morning, close:noon)
-c.shop = shop
-c.save
-
-d = BusinessHour.new(day:4,open:afternoon, close:evening)
-d.shop = shop
-d.save
+c = BusinessHour.create(day:4,open:morning, close:noon, shop: Shop.first)
+d = BusinessHour.create(day:4,open:afternoon, close:evening, shop: Shop.first)
 
 puts 'Vendredi...'
-e = BusinessHour.new(day:5,open:morning, close:noon)
-e.shop = shop
-e.save
-
-f = BusinessHour.new(day:5,open:afternoon, close:evening)
-f.shop = shop
-f.save
+BusinessHour.create(day:5,open:morning, close:noon, shop: Shop.first)
+BusinessHour.create(day:5,open:afternoon, close:evening, shop: Shop.first)
 
 
 puts 'Samedi...'
-g = BusinessHour.new(day:6,open:morning, close:evening)
-g.shop = shop
-g.save
+g = BusinessHour.create(day:6,open:morning, close:evening, shop: Shop.first)
+
 puts 'Samedi test...'
 
 # z = BusinessHour.new( shop:shop, day:6,open:t1, close:t2).valid?
 # puts "is valid? #{z}"
 
 puts 'Dimanche...'
-h = BusinessHour.new(day:0,opened:false)
-h.shop = shop
-h.save
+BusinessHour.create(day:0,opened:false, shop: Shop.first)
 
 puts 'Lundi...'
-i = BusinessHour.new(day:1,open:morning, close:noon)
-i.shop = shop
-i.save
-
-j = BusinessHour.new(day:1,open:afternoon, close:evening)
-j.shop = shop
-j.save
+BusinessHour.create(day:1,open:morning, close:noon, shop: Shop.first)
+BusinessHour.create(day:1,open:afternoon, close:evening, shop: Shop.first)
 
 
 puts 'Mardi...'
-k = BusinessHour.new(day:2,open:morning, close:noon)
-k.shop = shop
-k.save
-
-l = BusinessHour.new(day:2,open:afternoon, close:evening)
-l.shop = shop
-l.save
+BusinessHour.create(day:2,open:morning, close:noon, shop: Shop.first)
+BusinessHour.create(day:2,open:afternoon, close:evening, shop: Shop.first)

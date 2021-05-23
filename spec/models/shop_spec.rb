4bfdf4cae934cd699b9269aa69e1f70b8925c2bd
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-05-23 19:00:12 +0200

    Test - Shop: #array_ordered_days

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe Shop, type: :model do
       expect(shop).to eq(true)
     end
   end
+
+  context'method tests'do
+    describe '#array_ordered_days' do
+      it 'returns an array of days numbers sorted' do
+        shop = create(:shop)
+                
+        arr = case Date.today.wday
+              when 0
+                [0,1,2,3,4,5,6]
+              when 1
+                [1,2,3,4,5,6,0]
+              when 2
+                [2,3,4,5,6,0,1]
+              when 3
+                [3,4,5,6,0,1,2]
+              when 4
+                [4,5,6,0,1,2,3]
+              when 5
+                [5,6,0,1,2,3,4]
+              when 6
+                [6,0,1,2,3,4,5]
+              else
+                []
+              end
+
+        expect(shop.array_ordered_days).to eql(arr)
+      end
+    end
+  end
 end

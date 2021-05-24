0d47c138ef5809a4d3f5fa533964554d8d44db24
Frantz Crozilhac <fran.stacksta@gmail.com>
2021-05-24 18:10:18 +0200

    Update - Route friend & emergencies

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root to: 'shops#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :shops, only: :show, param: :slug
+  resources :shops, only: :show, param: :slug do 
+    resources :emergencies, controller: :contacts, type:'Emergency', only: [:new,:create,:edit,:update,:destroy ]
+    resources :friends, controller: :contacts, type:'Friend'
+  end
   resources :shops, only: :index
 
 end
